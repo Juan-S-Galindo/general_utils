6603b0e1dd11c92e311849d92c33cc5a48513a6b
JuanGalindo <sebastian@JuanGalindosMBP.attlocal.net>
2022-06-12 17:29:55 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="general_utils",
+    version="1.0",
+    description="Multiple classes and function for general use.",
+    author="Juan Galindo",
+    author_email="juan.s.galindo@outlook.com",
+    install_requires=[
+        "aws-lambda-powertools==1.26.0",
+        "boto3==1.24.2",
+        "requests==2.27.1",
+    ],
+)
