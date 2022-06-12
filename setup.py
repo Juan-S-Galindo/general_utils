1b14650913d3f999e0ebeb00e0b257af9e1cbf82
JuanGalindo <sebastian@JuanGalindosMBP.attlocal.net>
2022-06-12 18:05:50 -0500

    setup fix

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="general_utils",
     version="1.0",
     description="Multiple classes and function for general use.",
+    packages=['general_utils'],
+    package_dir={'':'src'},
     author="Juan Galindo",
     author_email="juan.s.galindo@outlook.com",
     install_requires=[
